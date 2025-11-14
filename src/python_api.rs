9ac5bee95a665a28e703ba5b117a8c5ea074ce99
Orhun Kok <59579802+OrhunKok@users.noreply.github.com>
2025-11-14 09:30:04 -0500

    Update python_api.rs

--- src/python_api.rs
+++ src/python_api.rs
@@ -1,9 +1,12 @@
 // Copyright 2021-2024 SecureDNA Stiftung (SecureDNA Foundation) <licensing@securedna.org>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use std::convert::TryFrom;
+
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
+use pyo3::wrap_pyfunction;
 
 use crate::{
     errors::TranslationError,
